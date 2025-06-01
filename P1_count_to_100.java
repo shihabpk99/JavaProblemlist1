6cfd8972522eadf72bc40a26ba6b844996dfc3b6
Shihab PK <150378687+shihabpk99@users.noreply.github.com>
2025-06-01 23:55:34 +0600

    3 problem done

--- P1_count_to_100.java
+++ P1_count_to_100.java
@@ -3,7 +3,8 @@ public class P1_count_to_100 {
     public static void main(String[] args) {
         // Loop from 1 to 100
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+            System.out.print(i+ " ");
+            
         }
     }
 }
